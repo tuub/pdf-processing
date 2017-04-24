7a1f99dc3d6947884770c50eb675ff4cf119f8ce
Per Broman <broman@tu-berlin.de>
2017-04-24 10:52:10 +0200

    Html title tag

--- elements/header.php
+++ elements/header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+        <title><?php echo($messages['htmlTitle'])?></title>
 		
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
