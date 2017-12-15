6217eff7f86204ec4d1c6a94fc41a9e0aece7c53
Per Broman <broman@tu-berlin.de>
2017-12-15 14:49:50 +0100

    GPL 3 license added

--- elements/footer.php
+++ elements/footer.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * (c) 2017 Technische Universität Berlin
+ *
+ * This software is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, 
+ * please see https://www.gnu.org/licenses/gpl-3.0.html or read 
+ * the LICENSE.txt file that was distributed with this source code.
+ */
+?>
         </div>
         <footer class="footer">
             <div class="container">
--- elements/metadata.php
+++ elements/metadata.php
@@ -1,5 +1,16 @@
 <?php
 /**
+ * (c) 2017 Technische Universität Berlin
+ *
+ * This software is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, 
+ * please see https://www.gnu.org/licenses/gpl-3.0.html or read 
+ * the LICENSE.txt file that was distributed with this source code.
+ */
+?>
+<?php
+/**
  * Input fields for additional metadata.
  */
 ?>
